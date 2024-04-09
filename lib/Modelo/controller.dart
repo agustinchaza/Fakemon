4bd0fe6e962b9d04070e3fd2149138f8a983f2a2
agusc <agustin.chazarreta@mi.unc.edu.ar>
2024-04-09 15:46:06 -0300

    V 1.0.0 CAMBIOS GRANDES - IMPLEMENTE GETX (falta definir logica de negocios)

--- lib/Modelo/controller.dart
+++ lib/Modelo/controller.dart
@@ -1,201 +1,209 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
 
+import 'AttackStrategy.dart';
 import 'Fakemon.dart';
 import 'tiposPokemon.dart';
 
 import 'dart:math';
 
-
-class ControllerBatalla extends GetxController  {
-
-
+class ControllerBatalla extends GetxController {
   RxString narradorDeBatalla = 'La batalla esta x comenzar'.obs;
 
-  RxBool turnoJugador = true.obs;
+  RxBool turnoJugador = false.obs;
+  RxBool batallaTermino = false.obs;
 
+  int indiceDelSwitch = 1;
+  int turnoActual = 1;
+  late Fakemon fakemonLento; //as Fakemon;
 
   //inicializacion por defecto, los valores deben ser cargados al iniciar la batalla
-  Rx <Fakemon> fakemonJugador = Fakemon(
+  Rx<Fakemon> fakemonJugador = Fakemon(
     strong: 100,
     speed: 100,
-    name: 'Charmander',
-    type: PokemonType.fire,
+    name: 'DEFECTO',
+    //type: PokemonType.fire,
     defensa: 100,
     hp: 100,
+    hpMAX: 100,
+    attacks: [
+      Attack(name: 'ThunderBolt', strategy: ThunderboltStrategy()),
+      Attack(name: 'QuickAttackStrategy', strategy: QuickAttackStrategy()),
+      Attack(name: 'IronTailStrategy', strategy: IronTailStrategy()),
+      Attack(name: 'EmberStrategy', strategy: EmberStrategy()),
+    ],
+  ).obs;
+
+  Rx<Fakemon> fakemonCPU = Fakemon(
+    strong: 100,
+    speed: 100,
+    name: 'DEFECTO',
+    //type: PokemonType.fire,
+    defensa: 100,
+    hp: 100,
+    hpMAX: 100,
     attacks: [],
   ).obs;
 
-  Rx <Fakemon> fakemonCPU = Fakemon(
-    strong: 100,
-    speed: 100,
-    name: 'Charmander',
-    type: PokemonType.fire,
-    defensa: 100,
-    hp: 100,
-    attacks: [],
-  ).obs;
-
-
-
-
-  setFakemons(Fakemon fakemonJugador, Fakemon fakemonCPU){
+  setFakemons(Fakemon fakemonJugador, Fakemon fakemonCPU) {
     this.fakemonJugador.value = fakemonJugador;
     this.fakemonCPU.value = fakemonCPU;
-
   }
 
-  getAcciones(int indexAttack){
-
-    if(!comprobarDebilitaciones(fakemonJugador.value)){
-      narradorDeBatalla.value = fakemonJugador.value.attack(fakemonCPU.value, indexAttack);
+  getAcciones(int indexAttack) {
+    if (!comprobarDebilitaciones(fakemonJugador.value)) {
+      narradorDeBatalla.value =
+          fakemonJugador.value.attack(fakemonCPU.value, indexAttack);
     }
+
+    flujoDeBatalla();
   }
 
-
-  ataqueCPU(){
-
-    if(!comprobarDebilitaciones(fakemonJugador.value)){
-
-      var rng = new Random();
+  ataqueCPU() {
+    if (!comprobarDebilitaciones(fakemonJugador.value)) {
+      var rng = Random();
       int indexAttack = rng.nextInt(fakemonCPU.value.attacks.length);
-      narradorDeBatalla.value = fakemonCPU.value.attack(fakemonJugador.value, indexAttack);
-
+      narradorDeBatalla.value =
+          fakemonCPU.value.attack(fakemonJugador.value, indexAttack);
+      update();
     }
-
   }
 
-
-  bool comprobarDebilitaciones(Fakemon fakemon){
-    if(fakemonJugador.value.estaConfundido){
-      narradorDeBatalla.value =  'El fakemon ${fakemon.name} esta confundido y no puede atacar';
+  bool comprobarDebilitaciones(Fakemon fakemon) {
+    if (fakemonJugador.value.estaConfundido) {
+      narradorDeBatalla.value =
+          'El fakemon ${fakemon.name} esta confundido y no puede atacar';
+      update();
       return true;
-    }
-    else if(fakemonJugador.value.estaParalizado){
-      narradorDeBatalla.value =  'El fakemon ${fakemon.name} esta paralizado y no puede atacar';
+    } else if (fakemonJugador.value.estaParalizado) {
+      narradorDeBatalla.value =
+          'El fakemon ${fakemon.name} esta paralizado y no puede atacar';
+      update();
       return true;
-    }
-    else if(fakemonJugador.value.estaDormido){
-      narradorDeBatalla.value =  'El fakemon ${fakemon.name} esta dormido y no puede atacar';
+    } else if (fakemonJugador.value.estaDormido) {
+      narradorDeBatalla.value =
+          'El fakemon ${fakemon.name} esta dormido y no puede atacar';
+      update();
       return true;
     }
 
     return false;
   }
 
-
-
-
-
-
-
-
-
-
-  bool comprobarVida(){
-    if(fakemonJugador.value.hp <= 0){
-      narradorDeBatalla.value = 'el Fakemon ${fakemonJugador.value.name} se debilito y a perdido la batalla. ${fakemonCPU.value.name} es el ganador!';
+  bool comprobarVida() {
+    if (fakemonJugador.value.hp <= 0) {
+      narradorDeBatalla.value =
+          'el Fakemon ${fakemonJugador.value.name} se debilito y a perdido la batalla. ${fakemonCPU.value.name} es el ganador!';
+      update();
       return true;
-    }
-    else if(fakemonCPU.value.hp <= 0){
-      narradorDeBatalla.value = 'el Fakemon ${fakemonCPU.value.name} se debilito y a perdido la batalla. ${fakemonJugador.value.name} es el ganador!';
+    } else if (fakemonCPU.value.hp <= 0) {
+      narradorDeBatalla.value =
+          'el Fakemon ${fakemonCPU.value.name} se debilito y a perdido la batalla. ${fakemonJugador.value.name} es el ganador!';
+      update();
       return true;
     }
     return false;
   }
 
-
-
-
-
-
+  //todo: rearmar esto, armar una fila de acciones a realizar y a cada llamada se realiza unade las acciones
   flujoDeBatalla() {
+    //switch case para las acciones a realizar
+    //1. comprobar estados del pokemon mas rapido
+    //2. comprobar estados del adversario
+    //3. ejecutar ataque del pokemon mas rapido
+    //4. ejecutar ataque del adversario
 
+    //a cada paso se debe comprobar si el pokemon esta debilitado y sumar 1 al indice del switch
+    switch (indiceDelSwitch) {
+      case 1:
+        if (fakemonJugador.value.speed > fakemonCPU.value.speed) {
+          comprobarEstados(fakemonJugador.value);
+          fakemonLento = fakemonCPU.value;
+        } else {
+          comprobarEstados(fakemonCPU.value);
+          fakemonLento = fakemonJugador.value;
+        }
+        indiceDelSwitch++;
+        break;
 
+      case 2:
+        comprobarEstados(fakemonLento);
+        indiceDelSwitch++;
+        break;
 
+      case 3:
+        if (fakemonJugador.value.speed > fakemonCPU.value.speed) {
+          turnoPlayer();
+          fakemonLento = fakemonCPU.value;
+        } else {
+          turnoCPU();
+          fakemonLento = fakemonJugador.value;
+        }
 
-    while (true) {
+        indiceDelSwitch++;
+        break;
 
-
-      fakemonJugador.value.speed > fakemonCPU.value.speed   ?    turnoJugador.value = true  :  turnoJugador.value = false;
-
-
-      if (turnoJugador.value) {
-
-        turnoPlayer();
-        if(comprobarVida()) break ;
-
-        turnoCPU();
-        if(comprobarVida()) break ;
-
-      }
-
-
-      else {
-
-        turnoCPU();
-        if(comprobarVida()) break ;
-
-        turnoPlayer();
-        if(comprobarVida()) break ;
-      }
-
-
-
-      for (var estado in fakemonJugador.value.estados) {
-        estado.actuar();
-        //TODO: esperar a recibir un stream de la vista
-        if(comprobarVida()) break ;
-      }
-
-      if(comprobarVida()) break ;
-
-      for (var estado in fakemonCPU.value.estados) {
-        estado.actuar();
-        //TODO: esperar a recibir un stream de la vista
-        if(comprobarVida()) break ;
-      }
-
-      if(comprobarVida()) break ;
-
-
-
-
+      case 4:
+        fakemonLento == fakemonJugador.value ? turnoPlayer() : turnoCPU();
+        indiceDelSwitch = 1; //reiniciar el ciclo
+        break;
+      default:
+        narradorDeBatalla.value = 'El programa fallo, error 1';
+        break;
     }
 
+    //TODO: esperar a recibir un stream de la v  ista para poder continuar
+
+    if (fakemonJugador.value.hp <= 0) {
+      sleep(const Duration(seconds: 5));
+      narradorDeBatalla.value =
+          'el Fakemon ${fakemonJugador.value.name} se debilito y a perdido la batalla. ${fakemonCPU.value.name} es el ganador!';
+    } else if (fakemonCPU.value.hp <= 0) {
+      sleep(const Duration(seconds: 5));
+      narradorDeBatalla.value =
+          'el Fakemon ${fakemonCPU.value.name} se debilito y a perdido la batalla. ${fakemonJugador.value.name} es el ganador!';
+    }
+
+    batallaTermino.value = true;
   }
 
+  void comprobarEstados(Fakemon f) {
+    for (var estado in f.estados) {
+      estado.actuar();
+      if (comprobarVida()) break;
+    }
+  }
 
+  void elegirAtaque(int index) {
+    fakemonJugador.value.attacks[index].strategy
+        .attack(fakemonJugador.value, fakemonCPU.value);
+    turnoJugador.value = false;
+    update();
+    print("hola");
+  }
 
   void turnoPlayer() {
-    narradorDeBatalla.value = 'Es el turno de ${fakemonJugador.value.name}. Elije un ataque';
-    int indexAttack=0 ;
-    //TODO: esperar a recibir un stream de la vista con el indice de ataque
-
-    getAcciones(indexAttack);
-
-    //TODO: esperar a recibir un stream de la vista para poder continuar
-
+    narradorDeBatalla.value =
+        'Es el turno de ${fakemonJugador.value.name}. Elije un ataque';
+    turnoJugador.value = true;
   }
 
   void turnoCPU() {
     narradorDeBatalla.value = 'Es el turno de ${fakemonCPU.value.name}.';
+    update();
 
-    //TODO: esperar a recibir un stream de la vista para poder continuar
-
+    sleep(const Duration(seconds: 3));
     ataqueCPU();
+  }
 
-    //TODO: esperar a recibir un stream de la vista para poder continuar
+  void setNarrador(String texto){
+    narradorDeBatalla.value = texto;
+    update();
+
 
   }
 
 
-
-
-
-
-
-
+  ControllerBatalla() {}
 }
-
-
-
