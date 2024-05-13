1c3a633ed9d9360c4c5dc6cc3ea19f1d162dc698
agusc <agustin.chazarreta@mi.unc.edu.ar>
2024-05-13 12:39:34 -0300

    V 1.4.2 actualice el README

--- README.md
+++ README.md
@@ -1,16 +1,44 @@
-# fakemon2
+# Fakemon
+Fakemon es más que un proyecto; 
+es un espacio de exploración diseñado para el aprendizaje y la práctica de diversas tecnologías,
+buenas prácticas, patrones de diseño y arquitectura en el desarrollo de aplicaciones móviles. 
+A través de este proyecto, nos sumergimos en un viaje de descubrimiento donde utilizamos herramientas modernas 
+y técnicas avanzadas para construir y mejorar nuestras habilidades como desarrolladores.
 
-A new Flutter project.
 
-## Getting Started
 
-This project is a starting point for a Flutter application.
+## Tecnologías utilizadas / exploradas
+- Flutter: Framework de código abierto creado por Google para construir aplicaciones nativas para iOS y Android desde una única base de código.
+- Dart: Lenguaje de programación utilizado para desarrollar aplicaciones Flutter.
+- GetX: Librería de estado de gestión, navegación y dependencias para Flutter.
+- Bloc: Patrón de gestión de estado para Flutter, que nos permite separar la lógica de negocio de la interfaz de usuario.
+- Mockito: Librería de Dart para crear mocks y tests unitarios.
+- GitHub: Plataforma de alojamiento de código para control de versiones y colaboración en proyectos de software.
+- GitHub Actions: Herramienta de CI/CD integrada en GitHub para automatizar tareas de construcción, pruebas y despliegue.
+- Vault: Herramienta de gestión de secretos utilizada para almacenar y controlar el acceso a información confidencial en nuestro proyecto.
 
-A few resources to get you started if this is your first Flutter project:
+## Estructura del proyecto
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+fakemon/  
+│  
+├── lib/  
+│   ├── assets/  
+│   ├── Modelo/  
+│   ├── vista/  
+│  
+├── test/   
+│  
+├── assets/  
+│  
+├── README.md  
+└── ...  
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+
+# Contribución
+¡Contribuciones son bienvenidas! Si deseas participar en el desarrollo de Fakemon, por favor sigue estos pasos:
+
+- Forkea el repositorio y clónalo en tu máquina local.
+- Crea una rama (git checkout -b feature/nueva-funcionalidad).
+- Realiza tus cambios y commitealos (git commit -am 'Agrega nueva funcionalidad').
+- Haz push a la rama (git push origin feature/nueva-funcionalidad).
+- Abre un Pull Request.
--- lib/vista/Menu_Inicial.dart
+++ lib/vista/Menu_Inicial.dart
@@ -4,17 +4,25 @@ import 'package:flutter/material.dart';
 
 import 'Menu_seleccion.dart';
 
+
+/// `Menu_Inicial` is a StatelessWidget that represents the initial menu of the application.
+/// It is implemented as a singleton, meaning there can only be one instance of this class.
 class Menu_Inicial extends StatelessWidget {
+  /// The single instance of `Menu_Inicial`.
   static final Menu_Inicial _instance = Menu_Inicial._internal();
 
+  /// Factory constructor that returns the single instance of `Menu_Inicial`.
   factory Menu_Inicial() {
     return _instance;
   }
 
+  /// Private constructor used to implement the singleton pattern.
   Menu_Inicial._internal();
 
+  /// A boolean value that determines whether the game can be played.
   bool _canPlay = true;
 
+  /// Builds the widget tree for `Menu_Inicial`.
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -37,19 +45,20 @@ class Menu_Inicial extends StatelessWidget {
                     Expanded(flex: 1, child: Container()),
                     Expanded(
                       flex: MediaQuery.of(context).orientation ==
-                              Orientation.portrait
+                          Orientation.portrait
                           ? 3
                           : 1,
                       child: ElevatedButton.icon(
+                        /// If `_canPlay` is true, navigate to `Menu_seleccion` when the button is pressed.
                         onPressed: _canPlay
                             ? () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => Menu_seleccion(),
-                                  ),
-                                );
-                              }
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Menu_seleccion(),
+                            ),
+                          );
+                        }
                             : null,
                         icon: Icon(Icons.settings_outlined,
                             color: Colors.yellow),
@@ -68,10 +77,11 @@ class Menu_Inicial extends StatelessWidget {
                     Expanded(flex: 2, child: Container()),
                     Expanded(
                       flex: MediaQuery.of(context).orientation ==
-                              Orientation.portrait
+                          Orientation.portrait
                           ? 3
                           : 1,
                       child: ElevatedButton.icon(
+                        /// Navigate to `batallaUI` when the button is pressed.
                         onPressed: () {
                           Navigator.push(
                             context,
@@ -81,7 +91,7 @@ class Menu_Inicial extends StatelessWidget {
                           );
                         },
                         icon:
-                            Icon(Icons.play_arrow_outlined, color: Colors.yellow),
+                        Icon(Icons.play_arrow_outlined, color: Colors.yellow),
 
                         style: ElevatedButton.styleFrom(
                           primary: Colors.blue,
@@ -105,7 +115,5 @@ class Menu_Inicial extends StatelessWidget {
         ],
       ),
     );
-
-
   }
 }
